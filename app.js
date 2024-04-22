e151099657d398e9ded7b77d6f2e3fe85d809a00
Pomidorchik34 <dimikabd95@gmail.com>
2024-04-22 23:01:38 +0300

    12

--- app.js
+++ app.js
@@ -1,49 +1,489 @@
-//MESSEGE
-let messege = " javascript ";
+// //MESSEGE
+// let messege = " javascript ";
 
-//INDEX OF
-let io = messege.indexOf("a");
-console.log(io);
+// //INDEX OF
+// let io = messege.indexOf("a");
+// console.log(io);
 
-//LAST INDEX OF
-let lio = messege.lastIndexOf("a");
-console.log(lio);
+// //LAST INDEX OF
+// let lio = messege.lastIndexOf("a");
+// console.log(lio);
 
-//SLICE
-let sli = messege.slice(4, 10);
-console.log(sli);
+// //SLICE
+// let sli = messege.slice(4, 10);
+// console.log(sli);
 
-//SUBSTRING
-let ssg = messege.substr(0, 4);
-console.log(ssg);
+// //SUBSTRING
+// let ssg = messege.substr(0, 4);
+// console.log(ssg);
 
-//REPLACE
-let rpe = messege.replace("java", "flowgoritm");
-console.log(rpe);
+// //REPLACE
+// let rpe = messege.replace("java", "flowgoritm");
+// console.log(rpe);
 
-//CHARAT
-let cat = messege.charAt(1);
-console.log(cat);
+// //CHARAT
+// let cat = messege.charAt(1);
+// console.log(cat);
 
-//TOUPERCASE
-let tee = messege.toUpperCase();
-console.log(tee);
+// //TOUPERCASE
+// let tee = messege.toUpperCase();
+// console.log(tee);
 
-//TOLOWERCASE
-let twe = messege.toLowerCase();
-console.log(twe);
+// //TOLOWERCASE
+// let twe = messege.toLowerCase();
+// console.log(twe);
 
-//TRIM
-let trm = messege.trim();
-console.log(trm);
+// //TRIM
+// let trm = messege.trim();
+// console.log(trm);
 
-//SPLIT
-let spt = messege.split(" ");
-console.log(spt);
+// //SPLIT
+// let spt = messege.split(" ");
+// console.log(spt);
 
 //HOMEWORK
-let ism = prompt("Ismingizni kiriting:");
-let birinchiHarf = ism.charAt().toUpperCase();
-let davomi = ism.slice(1).toLowerCase();
-let result = birinchiHarf + davomi;
-alert(`Salom, ${result}`);
+// let ism = prompt("Ismingizni kiriting:");
+// let birinchiHarf = ism.charAt().toUpperCase();
+// let davomi = ism.slice(1).toLowerCase();
+// let result = birinchiHarf + davomi;
+// alert(`Salom, ${result}`);
+
+//working
+
+// let d = 11000.34;
+// let y = 12354.03;
+// let samolyet = d * 500;
+// let mehmonhona = d * 250;
+// let bomj = y * 120;
+// let comfort = d * 750;
+// let econom = y * 120 + d * 500;
+
+// let ism = prompt("Ismingizni kiriting:");
+// let qiymat = prompt(`qancha pulingiz bor ${ism}`);
+// let idina = qiymat - econom;
+// let noo = idina - idina - idina;
+// if (qiymat >= econom) {
+//   alert("Oq yol");
+// } else {
+//   alert(
+//     `qadirli ${ism}
+//     sizga manshunacha mablag yemayapti ${noo} som`
+//   );
+// }
+
+//homework
+// if works
+//global
+// let n = Number(prompt(`soni kiriting`));
+
+//if 1
+// if (n > 0) {
+//   alert(n + 1);
+// }
+
+//if 2
+// if (n > 0) {
+//   alert(n + 1);
+// } else {
+//   alert(n + 2);
+// }
+
+//if3
+// if (n > 0) {
+//   alert(n + 1);
+// } else if (n == 0) {
+//   alert((n = 10));
+// } else {
+//   n - 2;
+// }
+
+// GLOBAL
+// let n1 = Number(prompt(`1-soni kiritng`));
+// let n2 = Number(prompt(`2-soni kiritng`));
+// let n3 = Number(prompt(`3-soni kiritng`));
+
+// let manfiy = 0;
+// let musbat = 0;
+// // if4 && if5
+// if (n1 > 0) {
+//   musbat++;
+// } else {
+//   manfiy++;
+// }
+// if (n2 > 0) {
+//   musbat++;
+// } else {
+//   manfiy++;
+// }
+// if (n3 > 0) {
+//   musbat++;
+// } else {
+//   manfiy++;
+// }
+// alert(`manfiy son ${manfiy}ta musbat son ${musbat}ta`);
+
+//if 6
+// if (n1 > n2) {
+//   alert(`1-son katta`);
+// } else {
+//   alert(`2-son katta`);
+// }
+
+//if7
+// if (n1 < n2) {
+//   alert(`1-son kichik`);
+// } else {
+//   alert(`2-son kichik`);
+// }
+
+//if8
+// if (n1 > n2) {
+//   alert(`${n2} ${n1}`);
+// } else {
+//   alert(`${n1} ${n2}`);
+// }
+
+//if9
+// if (n1 > n2) {
+//   alert(`${n2}, ${n1} `);
+// } else {
+//   alert(`${n1}, ${n2} `);
+// }
+
+//if10
+// if (n1 != n2) {
+//   alert(`a=${n1 + n2}, b=${n1 + n2}`);
+// } else {
+//   alert(`0 , 0 `);
+// }
+
+//if11
+// if (n1 > n2) {
+//   alert(`${n1}, ${n1}`);
+// } else if (n2 > n1) {
+//   alert(`${n2}, ${n2}`);
+// } else {
+//   alert(`0, 0`);
+// }
+
+//if12
+// if (n1 <= n2 && n1 <= n3) {
+//   alert(n1);
+// } else if (n2 <= n3 && n2 <= n1) {
+//   alert(n2);
+// } else if (n3 <= n2 && n3 <= n1) {
+//   alert(n3);
+// } else {
+//   alert("Hello world");
+// }
+
+//if13
+// if ((n1 <= n2 && n1 >= n3) || (n1 >= n2 && n1 <= n3)) {
+//   alert(n1);
+// } else if ((n2 <= n3 && n2 >= n1) || (n2 >= n3 && n2 <= n1)) {
+//   alert(n2);
+// } else if ((n3 <= n2 && n3 >= n1) || (n3 >= n2 && n3 <= n1)) {
+//   alert(n3);
+// } else {
+//   alert("Hello world");
+// }
+
+//if14
+// if (n1 <= n2 && n1 >= n3) {
+//   alert(`${n3} ${n2}`);
+// } else if (n1 >= n2 && n1 <= n3) {
+//   alert(`${n2} ${n3}`);
+// } else if (n2 <= n3 && n2 >= n1) {
+//   alert(`${n1}, ${n3}`);
+// } else if (n2 >= n3 && n2 <= n1) {
+//   alert(`${n3}, ${n1}`);
+// } else if (n3 <= n2 && n3 >= n1) {
+//   alert(`${n2}, ${n1}`);
+// } else if (n3 >= n2 && n3 <= n1) {
+//   alert(`${n1}, ${n2}`);
+// } else {
+//   alert("Hello world");
+// }
+
+//if15
+//   alert(`${n1} ${n2}`);
+// } else if (n1 >= n2 && n1 <= n3) {
+//   alert(`${n1} ${n3}`);
+// } else if (n2 <= n3 && n2 >= n1) {
+//   alert(`${n2}, ${n3}`);
+// } else if (n2 >= n3 && n2 <= n1) {
+//   alert(`${n2}, ${n1}`);
+// } else if (n3 <= n2 && n3 >= n1) {
+//   alert(`${n3}, ${n1}`);
+// } else if (n3 >= n2 && n3 <= n1) {
+//   alert(`${n3}, ${n2}`);
+// } else {
+//   alert("Hello world");
+// }
+
+//NOT IF
+
+//BOLEAN
+
+//GLOBAL
+
+//N1 N2
+// let n = Number(prompt("1- soni kiriting"));
+// let n2 = Number(prompt("2- soni kiriting"));
+
+//ABC
+// let a = Number(prompt("a Son kiriting"));
+// let b = Number(prompt("b Son kiriting"));
+// let c = Number(prompt("c Son kiriting"));
+
+//bolean1
+// if (n > 0) {
+//   alert("Bu musbat son");
+// } else {
+//   alert("Bu musbat son emas");
+// }
+
+//bolean2
+// if (num % 2 == 1) {
+//   alert("Bu toq son");
+// } else {
+//   alert("Bu toq son emas");
+// }
+
+//bolean3
+// if (num % 2 == 0) {
+//   alert("Bu juft son");
+// } else {
+//   alert("Bu juft son emas");
+// }
+
+//bolean4
+// if (a > 2 && b <= 3) {
+//   alert("A 2 dan katta va B 3dan kichik yoki teng");
+// } else {
+//   alert("A 2 dan katta emas va B 3dan kichik yoki teng emas");
+// }
+
+//bolean5
+// if (a >= 0 && b < -2) {
+//   alert("A 0 dan katta yoki teng va B -2 dan kichik ");
+// } else {
+//   alert("A 0 dan katta yoki teng emas va B -2 dan kichik emas");
+// }
+
+//bolean6
+// if (a <= b && b <= c) {
+//   alert(true );
+// } else {
+//   alert(false);
+// }
+
+//bolean7
+
+// if ((a < b && b < c) || (c < b && b < a)) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean8
+
+// if (a % 2 == 1 && b % 2 == 1) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean9
+
+// if (a % 2 == 1 || b % 2 == 1) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+//bolean10
+
+// if ((a % 2 == 1 && b % 2 == 0) || (a % 2 == 0 && b % 2 == 1)) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean11
+// if ((a % 2 == 1 && b % 2 == 1) || (a % 2 == 0 && b % 2 == 0)) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean12
+// if (a > 0 && b > 0 && c > 0) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean13
+// if (a > 0 || b > 0 || c > 0) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean14
+// if (
+//   (a > 0 && b < 0 && c < 0) ||
+//   (a < 0 && b > 0 && c < 0) ||
+//   (a < 0 && b < 0 && c > 0)
+// ) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean15
+// if (
+//   (a > 0 && b > 0 && c < 0) ||
+//   (a < 0 && b > 0 && c > 0) ||
+//   (a > 0 && b < 0 && c > 0)
+// ) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean16
+// if (n % 2 == 0 && n > 9 && n < 100) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean17
+// if (n % 2 == 1 && n > 99 && n < 1000) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean18
+// if (a == b || a == c || c == b) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean19
+// if (a == -b || a == -c || c == -b) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean20
+// if (a != b && a != c && c != b) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean21
+// if (a > b && a > c && c < b) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean22
+// let a = n % 10;
+// let b = ((n % 100) - (n % 10)) / 10;
+// let c = (n - (n % 100)) / 100;
+// if ((a > b && a > c && c < b) || (a < b && a < c && c > b)) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//bolean23
+// let n = Number(prompt("3 xonali son kiriting"));
+
+// let a = n % 10;
+// let b = ((n % 100) - (n % 10)) / 10;
+// let c = (n - (n % 100)) / 100;
+// if (a == c && n > 99 && n < 1000) {
+//   alert(true);
+// } else {
+//   alert(false);
+// }
+
+//FOOOOOOR
+//global
+let n = prompt(`soni kiriting`);
+
+//for1
+// for (let i = 0; i < n; i++) {
+//   alert(`${i} press enter`);
+// }
+
+//for2
+// for (let i = n; i > 1; i--) {
+//   alert(`${i} press enter`);
+// }
+
+//for3
+// let sum = 0;
+// for (let i = 0; i < n; i++) {
+//   sum++;
+//   sum = i + sum++;
+//   alert(`${sum} press enter`);
+// }
+
+//for4 & 5
+// let juft = 0;
+// let toq = 0;
+// for (let i = 0; i < n; i++) {
+//   if (i % 2 == 0) {
+//     juft++;
+//   } else {
+//     toq++;
+//   }
+// }
+// alert(`${juft}ta juft son va ${toq}ta toq son bor`);
+
+//for6
+// let counter = 0;
+// for (let i = 0; i < n; i++) {
+//   if (n % i == 0) {
+//     counter++;
+//   }
+// }
+// alert(`${n}ni ${counter}ta boluvchisi bor`);
+
+//for7
+// let sum = 0;
+// let counter = 0;
+// for (let i = 0; i < n; i++) {
+//   if (n % i == 0) {
+//     counter++;
+//     sum = sum + i;
+//   }
+// }
+// alert(`${n}ni ${sum} boluvchilarning yeg'indisi`);
+
+//for8
+let counter = 0;
+let allCouunter = 0;
+for (let i = 1; i <= n; i++) {
+  for (let k = 1; k <= i; k++) {
+    if (i % k == 0) {
+      counter++;
+    }
+  }
+  if (counter == 2) {
+    allCouunter++;
+  }
+  k = 1;
+  i = i + 1;
+  counter = 0;
+}
+alert(`0dan ${n}gacha ${allCouunter}ta tub son mavjud`);
